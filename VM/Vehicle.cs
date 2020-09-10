a63e8786babd65640e1aae3b6b9f98d0eaf7ae4c
laxmi-pgconnect <laxmi.pgconnect@gmail.com>
2020-09-10 11:42:48 +0530

    Modified Vehicles

--- VM/Vehicle.cs
+++ VM/Vehicle.cs
@@ -9,12 +9,6 @@ namespace ReactPPD.VM
     {
         public string VehCode { get; set; }        
         public string VehNo { get; set; }        
-        public string VehType { get; set; }
-        public string MakeName { get; set; }        
-        public string DivCode { get; set; }
-        public string DivName{ get; set; }
-        public string RegionCode { get; set; }
-        public string RegionName { get; set; }
-        public string IsActive { get; set; }
+       
     }
 }
